80ea651208cf831f6d964f58bdfa0dcb1cffc91c
Alexander Schuessler <schuessler.alex@gmail.com>
2022-05-16 12:40:46 -0500

    commit tree 2132af907a023cd76550731785cc2cfe884f23fe

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index do |dwarf,index|
+    puts "#{index+1}: #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.map do |element|
+  element[0].upcase + element.slice(1,element.length) + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(arr)
+  filtered = arr.filter { |word| word.length >4}
+  if filtered.length == 0
+    false
+  else
+    true
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  numbers = []
+  cheese_types.each do |cheese|
+   numbers.push(ingredients.find_index { |ingredient,index| ingredient.include?(cheese)})
+   numbers.filter { |number| number!= nil}
+  end
+
+  numbers.filter! { |number| number!= nil}
+  if numbers.length == 0
+    nil
+  else
+  ingredients[numbers.sort[0]]
+  end
 end
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(soup)
